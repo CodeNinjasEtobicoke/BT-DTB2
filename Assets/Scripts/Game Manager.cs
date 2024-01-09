04c8900b507df718f2be75d16891a93f53d6fc10
I Love Iced Coffee <107214218+CodeNinjasEtobicoke@users.noreply.github.com>
2024-01-09 17:46:47 -0500

    hai

---
+++ Assets/Scripts/Game Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Purple belt is hard (;-;)
+    private Spawner spawner;
+    private void Awake()
+    {
+        spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+    }
+    void Start()
+    {
+        spawner.active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+            spawner.active = false;
+    }
+}
